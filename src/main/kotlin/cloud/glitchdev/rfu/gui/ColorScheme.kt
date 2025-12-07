73cd97029459c3967f61aa154b5f8a0d1ea5045e
Riccio <166852556+Ricciow@users.noreply.github.com>
2025-12-07 12:22:54 -0300

    feat: adjust color scheme

--- src/main/kotlin/cloud/glitchdev/rfu/gui/ColorScheme.kt
+++ src/main/kotlin/cloud/glitchdev/rfu/gui/ColorScheme.kt
@@ -6,11 +6,11 @@ object ColorScheme {
     val primaryColor = Color(37, 114, 153)
     val primaryColorOpaque = increaseOpacity(primaryColor, 127)
 
-    val secondaryColor = Color(28, 85, 115)
+    val secondaryColor = Color(200, 200, 200)
     val secondaryColorOpaque = increaseOpacity(secondaryColor, 127)
 
     val primaryTextColor = Color(255, 255, 255)
-    val secondaryTextColor = Color(127, 127, 127)
+    val secondaryTextColor = Color(180, 180, 180)
 
     fun increaseOpacity(baseColor : Color, amount: Int) : Color {
         return Color(baseColor.red, baseColor.green, baseColor.blue, baseColor.alpha - amount)
