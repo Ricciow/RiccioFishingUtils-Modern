3f97792c0fef530d67a5d9fb6d5c099b2ec635bf
Riccio <riccio.fernando@gmail.com>
2025-12-08 12:38:48 -0300

    fix: remove unused methods

--- src/main/kotlin/cloud/glitchdev/rfu/utils/dsl/gui.kt
+++ src/main/kotlin/cloud/glitchdev/rfu/utils/dsl/gui.kt
@@ -1,14 +1,8 @@
 package cloud.glitchdev.rfu.utils.dsl
 
 import gg.essential.elementa.UIComponent
-import gg.essential.elementa.constraints.CenterConstraint
-import gg.essential.elementa.constraints.ChildBasedSizeConstraint
 import gg.essential.elementa.constraints.ColorConstraint
-import gg.essential.elementa.constraints.PixelConstraint
-import gg.essential.elementa.constraints.RelativeConstraint
-import gg.essential.elementa.constraints.SuperConstraint
 import gg.essential.elementa.constraints.animation.AnimationStrategy
-import gg.essential.elementa.constraints.animation.Animations
 import gg.essential.elementa.dsl.animate
 
 fun <T : UIComponent> T.setHidden(state: Boolean) : UIComponent {
@@ -33,25 +27,5 @@ fun <T : UIComponent> T.addHoverColoring(strategy: AnimationStrategy, duration :
         }
     }
 
-    return this
-}
-
-fun PixelConstraint.changeTarget(target: UIComponent) : PixelConstraint {
-    this.constrainTo = target
-    return this
-}
-
-fun RelativeConstraint.changeTarget(target: UIComponent) : RelativeConstraint {
-    this.constrainTo = target
-    return this
-}
-
-fun CenterConstraint.changeTarget(target: UIComponent) : CenterConstraint {
-    this.constrainTo = target
-    return this
-}
-
-fun ChildBasedSizeConstraint.changeTarget(target: UIComponent) : ChildBasedSizeConstraint {
-    this.constrainTo = target
     return this
 }
