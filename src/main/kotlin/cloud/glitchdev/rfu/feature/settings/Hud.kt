e19ed47cc6dc88fdfcf690ba58bed46bb34a171a
Riccio <riccio.fernando@gmail.com>
2026-02-13 10:34:06 -0300

    fix: rfumove wasnt initialized

--- src/main/kotlin/cloud/glitchdev/rfu/feature/settings/Hud.kt
+++ src/main/kotlin/cloud/glitchdev/rfu/feature/settings/Hud.kt
@@ -1,10 +1,12 @@
 package cloud.glitchdev.rfu.feature.settings
 
 import cloud.glitchdev.rfu.gui.window.HudWindow
+import cloud.glitchdev.rfu.utils.command.Command
 import cloud.glitchdev.rfu.utils.command.SimpleCommand
 import com.mojang.brigadier.context.CommandContext
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
 
+@Command
 object Hud : SimpleCommand("rfumove") {
     override val description: String = "Opens the GUI to move Hud Elements."
 
