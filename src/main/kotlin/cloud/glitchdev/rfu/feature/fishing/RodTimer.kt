18bf6ff0f91a3eae6228e50d10c60655dab7c82d
Riccio <riccio.fernando@gmail.com>
2026-02-17 18:47:04 -0300

    perf: optimize searching so it only happens when holding a fishing rod and no entity was found before

--- src/main/kotlin/cloud/glitchdev/rfu/feature/fishing/RodTimer.kt
+++ src/main/kotlin/cloud/glitchdev/rfu/feature/fishing/RodTimer.kt
@@ -1,5 +1,7 @@
 package cloud.glitchdev.rfu.feature.fishing
 
+import cloud.glitchdev.rfu.RiccioFishingUtils.mc
+import cloud.glitchdev.rfu.config.categories.GeneralFishing
 import cloud.glitchdev.rfu.events.managers.TickEvents.registerTickEvent
 import cloud.glitchdev.rfu.feature.Feature
 import cloud.glitchdev.rfu.feature.RFUFeature
@@ -11,18 +13,22 @@ import net.minecraft.world.entity.decoration.ArmorStand
 @RFUFeature
 object RodTimer : Feature {
     val timerRegex = """!!!|\d\.\d""".toExactRegex()
+    var timer : ArmorStand? = null
 
     override fun onInitialize() {
         registerTickEvent(0, 2) { client ->
+            if(!GeneralFishing.rodTimerDisplay) return@registerTickEvent
             val world = client.level ?: return@registerTickEvent
 
-            val entity = world.entitiesForRendering().find { entity ->
-                if(entity !is ArmorStand) return@find false
-                if (!entity.hasCustomName()) return@find false
-                return@find entity.name.toUnformattedString().matches(timerRegex)
-            } as? ArmorStand
+            if(timer?.isRemoved ?: true && mc.player?.mainHandItem?.item?.descriptionId == "item.minecraft.fishing_rod") {
+                timer = world.entitiesForRendering().find { entity ->
+                    if(entity !is ArmorStand) return@find false
+                    if (!entity.hasCustomName()) return@find false
+                    return@find entity.name.toUnformattedString().matches(timerRegex)
+                } as? ArmorStand
+            }
 
-            RodTimerDisplay.rodTime = entity?.name?.string?.toFloatOrNull() ?: -1f
+            RodTimerDisplay.rodTime = timer?.name?.string?.toFloatOrNull() ?: -1f
             RodTimerDisplay.updateState()
         }
     }
