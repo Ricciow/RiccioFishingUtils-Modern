c0cb26a6042f5a121d006c6f676601c325058aef
Riccio <166852556+Ricciow@users.noreply.github.com>
2026-02-19 17:12:30 -0300

    feat: add descriptions to party msg

--- src/main/kotlin/cloud/glitchdev/rfu/feature/partyfinder/PartyFinderAlert.kt
+++ src/main/kotlin/cloud/glitchdev/rfu/feature/partyfinder/PartyFinderAlert.kt
@@ -1,5 +1,6 @@
 package cloud.glitchdev.rfu.feature.partyfinder
 
+import cloud.glitchdev.rfu.config.categories.BackendSettings
 import cloud.glitchdev.rfu.config.categories.OtherSettings
 import cloud.glitchdev.rfu.constants.text.TextColor.*
 import cloud.glitchdev.rfu.events.managers.TickEvents.registerTickEvent
@@ -9,6 +10,10 @@ import cloud.glitchdev.rfu.utils.Chat
 import cloud.glitchdev.rfu.utils.TextUtils
 import cloud.glitchdev.rfu.utils.network.PartyHttp
 import cloud.glitchdev.rfu.model.party.FishingParty
+import net.minecraft.network.chat.ClickEvent
+import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.HoverEvent
+import net.minecraft.network.chat.Style
 
 @RFUFeature
 object PartyFinderAlert : Feature {
@@ -17,6 +22,7 @@ object PartyFinderAlert : Feature {
 
     override fun onInitialize() {
         registerTickEvent(interval = 6000) {
+            if (!BackendSettings.backendAccepted) return@registerTickEvent
             if (!OtherSettings.partyFinderAlert) return@registerTickEvent
 
             PartyHttp.getParties { parties ->
@@ -40,6 +46,11 @@ object PartyFinderAlert : Feature {
 
         if (newParties.isNotEmpty()) {
             val message = TextUtils.rfupfLiteral("There are $WHITE${newParties.size}$GOLD new parties!",GOLD)
+                .setStyle(
+                    Style.EMPTY
+                        .withHoverEvent(HoverEvent.ShowText(Component.literal("Open party finder /rfupf\n§8You can disable this message in the settings!\n§8Other -> Party Finder Alert")))
+                        .withClickEvent(ClickEvent.RunCommand("rfupf"))
+                )
             Chat.sendMessage(message)
         }
 
