44a243a786bbb8ef123cb67c6943cba51bd4a9b0
Riccio <riccio.fernando@gmail.com>
2026-03-06 09:49:53 -0300

    fix: make lootshare events only trigger with inventory/hotbar

--- src/main/kotlin/cloud/glitchdev/rfu/events/managers/LootshareEvents.kt
+++ src/main/kotlin/cloud/glitchdev/rfu/events/managers/LootshareEvents.kt
@@ -49,6 +49,8 @@ object LootshareEvents : AbstractEventManager<(contributors: List<String>, items
 
         registerSetSlotEvent { containerId, slot, item ->
             if (containerId != PLAYER_INVENTORY_ID || item.isEmpty) return@registerSetSlotEvent
+            //Hotbar/Inventory
+            if (slot !in 9..44) return@registerSetSlotEvent
 
             val player = Minecraft.getInstance().player ?: return@registerSetSlotEvent
             val previousItem = player.inventoryMenu.getSlot(slot).item
