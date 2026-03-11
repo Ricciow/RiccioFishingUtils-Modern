7b90ff154712bd2b97668171d77b26b4e7ce6413
Riccio <166852556+Ricciow@users.noreply.github.com>
2026-03-10 23:51:39 -0300

    feat: add treasure dye achievement

---
+++ src/main/kotlin/cloud/glitchdev/rfu/achievement/achievements/special/TenaciousTreasureTrawler.kt
@@ -0,0 +1,27 @@
+package cloud.glitchdev.rfu.achievement.achievements.special
+
+import cloud.glitchdev.rfu.achievement.Achievement
+import cloud.glitchdev.rfu.achievement.AchievementCategory
+import cloud.glitchdev.rfu.achievement.AchievementDifficulty
+import cloud.glitchdev.rfu.achievement.AchievementType
+import cloud.glitchdev.rfu.achievement.BaseAchievement
+import cloud.glitchdev.rfu.constants.Dyes
+import cloud.glitchdev.rfu.events.managers.DropEvents.registerDyeDropEvent
+
+@Achievement
+object TenaciousTreasureTrawler : BaseAchievement() {
+    override val id: String = "tenacious_treasure_trawler"
+    override val name: String = "Tenacious Treasure Trawler"
+    override val description: String = "Drop a Treasure Dye."
+    override val type: AchievementType = AchievementType.NORMAL
+    override val difficulty: AchievementDifficulty = AchievementDifficulty.IMPOSSIBLE
+    override val category: AchievementCategory = AchievementCategory.SPECIAL
+
+    override fun setupListeners() {
+         activeListeners.add(registerDyeDropEvent { dyeDrop, _ ->
+            if (dyeDrop == Dyes.TREASURE) {
+                complete()
+            }
+        })
+    }
+}
