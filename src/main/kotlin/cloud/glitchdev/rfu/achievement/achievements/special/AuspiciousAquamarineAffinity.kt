bb621444f2aa8d2a83e88d4fb01229c79f557c7b
Riccio <166852556+Ricciow@users.noreply.github.com>
2026-03-10 23:41:12 -0300

    feat: add aquamarine dye achievement

---
+++ src/main/kotlin/cloud/glitchdev/rfu/achievement/achievements/special/AuspiciousAquamarineAffinity.kt
@@ -0,0 +1,27 @@
+package cloud.glitchdev.rfu.achievement.achievements.special
+
+import cloud.glitchdev.rfu.achievement.Achievement
+import cloud.glitchdev.rfu.achievement.AchievementCategory
+import cloud.glitchdev.rfu.achievement.AchievementDifficulty
+import cloud.glitchdev.rfu.achievement.AchievementType
+import cloud.glitchdev.rfu.achievement.BaseAchievement
+import cloud.glitchdev.rfu.constants.Dyes
+import cloud.glitchdev.rfu.events.managers.DropEvents.registerDyeDropEvent
+
+@Achievement
+object AuspiciousAquamarineAffinity : BaseAchievement() {
+    override val id: String = "auspicious_aquamarine_affinity"
+    override val name: String = "Auspicious Aquamarine Affinity"
+    override val description: String = "Drop an Aquamarine Dye."
+    override val type: AchievementType = AchievementType.NORMAL
+    override val difficulty: AchievementDifficulty = AchievementDifficulty.IMPOSSIBLE
+    override val category: AchievementCategory = AchievementCategory.SPECIAL
+
+    override fun setupListeners() {
+        registerDyeDropEvent { dyeDrop, _ ->
+            if(dyeDrop == Dyes.AQUAMARINE) {
+                complete()
+            }
+        }
+    }
+}
--- src/main/kotlin/cloud/glitchdev/rfu/achievement/achievements/special/CrazyCarmineConosseurAchievement.kt
+++ src/main/kotlin/cloud/glitchdev/rfu/achievement/achievements/special/CrazyCarmineConosseurAchievement.kt
@@ -1,4 +1,4 @@
-package cloud.glitchdev.rfu.achievement.achievements.isle
+package cloud.glitchdev.rfu.achievement.achievements.special
 
 import cloud.glitchdev.rfu.achievement.Achievement
 import cloud.glitchdev.rfu.achievement.AchievementCategory
@@ -6,20 +6,20 @@ import cloud.glitchdev.rfu.achievement.AchievementDifficulty
 import cloud.glitchdev.rfu.achievement.AchievementType
 import cloud.glitchdev.rfu.achievement.BaseAchievement
 import cloud.glitchdev.rfu.constants.Dyes
-import cloud.glitchdev.rfu.events.managers.DropEvents.registerDyeDropEvent
+import cloud.glitchdev.rfu.events.managers.DropEvents
 
 @Achievement
 object CrazyCarmineConosseurAchievement : BaseAchievement() {
     override val id: String = "crazy_carmine_conosseur"
     override val name: String = "Crazy Carmine Conosseur"
-    override val description: String = "Drop a carmine dye."
+    override val description: String = "Drop a Carmine Dye."
     override val type: AchievementType = AchievementType.NORMAL
     override val difficulty: AchievementDifficulty = AchievementDifficulty.IMPOSSIBLE
     override val category: AchievementCategory = AchievementCategory.SPECIAL
 
     override fun setupListeners() {
-        registerDyeDropEvent { dyeDrop, _ ->
-            if(dyeDrop == Dyes.CARMINE) {
+        DropEvents.registerDyeDropEvent { dyeDrop, _ ->
+            if (dyeDrop == Dyes.CARMINE) {
                 complete()
             }
         }
