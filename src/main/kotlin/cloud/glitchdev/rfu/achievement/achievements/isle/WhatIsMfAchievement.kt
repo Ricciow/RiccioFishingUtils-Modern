8fb99f25f11ac3832904aed241c6f2c776c6a974
Riccio <166852556+Ricciow@users.noreply.github.com>
2026-03-12 17:34:59 -0300

    feat: add what is mf achievement

---
+++ src/main/kotlin/cloud/glitchdev/rfu/achievement/achievements/isle/WhatIsMfAchievement.kt
@@ -0,0 +1,30 @@
+package cloud.glitchdev.rfu.achievement.achievements.isle
+
+import cloud.glitchdev.rfu.achievement.Achievement
+import cloud.glitchdev.rfu.achievement.AchievementCategory
+import cloud.glitchdev.rfu.achievement.AchievementDifficulty
+import cloud.glitchdev.rfu.achievement.AchievementType
+import cloud.glitchdev.rfu.achievement.BaseAchievement
+import cloud.glitchdev.rfu.constants.RareDrops
+import cloud.glitchdev.rfu.events.managers.DropEvents.registerRareDropEvent
+
+@Achievement
+object WhatIsMfAchievement : BaseAchievement() {
+    override val id: String = "what_is_mf"
+    override val name: String = "What is Magic Find?"
+    override val description: String = "Drop a Radioactive Vial with 150 Magic Find or less"
+    override val type: AchievementType = AchievementType.NORMAL
+    override val difficulty: AchievementDifficulty = AchievementDifficulty.HARD
+    override val category: AchievementCategory = AchievementCategory.ISLE
+
+    private val DROP = RareDrops.RADIOACTIVE_VIAL
+
+    override fun setupListeners() {
+        activeListeners.add(registerRareDropEvent { drop, mf ->
+            if(drop == DROP && (mf ?: 0) <= 150) {
+                complete()
+            }
+            true
+        })
+    }
+}
