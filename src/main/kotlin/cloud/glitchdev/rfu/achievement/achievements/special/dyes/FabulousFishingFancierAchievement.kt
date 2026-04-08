1145fe8532ae204541d8d87142e40a4c50fcfc92
Riccio <166852556+Ricciow@users.noreply.github.com>
2026-04-08 14:30:58 -0300

    feat: add bone dye achievement

--- src/main/kotlin/cloud/glitchdev/rfu/achievement/achievements/special/dyes/FabulousFishingFancierAchievement.kt
+++ src/main/kotlin/cloud/glitchdev/rfu/achievement/achievements/special/dyes/FabulousFishingFancierAchievement.kt
@@ -14,7 +14,7 @@ object FabulousFishingFancierAchievement : CompletionAchievement() {
         TenaciousTreasureTrawlerAchievement)
     override val id: String = "fabulous_fishing_fancier"
     override val name: String = "Fabulous Fishing Fancier"
-    override val description: String = "Get all 5 fishing dyes"
+    override val description: String = "Get all 5 true fishing dyes"
     override val type: AchievementType = AchievementType.SECRET
     override val difficulty: AchievementDifficulty = AchievementDifficulty.IMPOSSIBLE
     override val category: AchievementCategory = AchievementCategory.SPECIAL
