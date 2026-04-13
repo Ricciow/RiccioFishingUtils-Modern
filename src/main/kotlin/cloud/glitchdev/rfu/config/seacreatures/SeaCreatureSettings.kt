15ea685bd9ad11a633ed23f390e2bcb2600f9326
Riccio <riccio.fernando@gmail.com>
2026-04-12 23:53:33 -0300

    style: format

--- src/main/kotlin/cloud/glitchdev/rfu/config/seacreatures/SeaCreatureSettings.kt
+++ src/main/kotlin/cloud/glitchdev/rfu/config/seacreatures/SeaCreatureSettings.kt
@@ -2,13 +2,10 @@ package cloud.glitchdev.rfu.config.seacreatures
 
 data class SeaCreatureSettings(
     val creatures: Map<String, SeaCreatureSetting>
-)
-
-{
+) {
     companion object {
         fun empty(): SeaCreatureSettings = SeaCreatureSettings(mapOf())
     }
-
 }
 
 data class SeaCreatureSetting(
