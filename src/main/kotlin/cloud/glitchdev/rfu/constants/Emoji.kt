c8e9be049b50af1759c9cd4a878c24de65a846f6
Riccio <166852556+Ricciow@users.noreply.github.com>
2026-04-24 02:05:49 -0300

    fix: make emoji always use white color

--- src/main/kotlin/cloud/glitchdev/rfu/constants/Emoji.kt
+++ src/main/kotlin/cloud/glitchdev/rfu/constants/Emoji.kt
@@ -1,9 +1,16 @@
 package cloud.glitchdev.rfu.constants
 
+import cloud.glitchdev.rfu.constants.text.TextColor
+import cloud.glitchdev.rfu.constants.text.TextEffects
+
 object Emoji {
     const val DOG_CHAR = "\uE001"
 
     val ALL = mapOf(
-        ":dog:" to DOG_CHAR
+        ":dog:" to DOG_CHAR.whiteText(),
     )
+
+    fun String.whiteText() : String {
+        return "${TextColor.WHITE}$this${TextEffects.RESET}"
+    }
 }
