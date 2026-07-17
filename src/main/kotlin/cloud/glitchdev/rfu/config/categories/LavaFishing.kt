d551d386569445bf64eab6802889e4c0d1cdd118
Riccio <166852556+Ricciow@users.noreply.github.com>
2026-07-17 15:43:50 -0300

    feat: vanquisher party message

--- src/main/kotlin/cloud/glitchdev/rfu/config/categories/LavaFishing.kt
+++ src/main/kotlin/cloud/glitchdev/rfu/config/categories/LavaFishing.kt
@@ -1,6 +1,7 @@
 package cloud.glitchdev.rfu.config.categories
 
 import cloud.glitchdev.rfu.config.Category
+import cloud.glitchdev.rfu.feature.fishing.VanquisherPartyMessage
 
 object LavaFishing : Category("Lava Fishing") {
     init {
@@ -44,5 +45,43 @@ object LavaFishing : Category("Lava Fishing") {
     var plhlegblastGlow by boolean(true) {
         name = Literal("Plhlegblast Glow")
         description = Literal("Visually transforms Squids named 'Plhlegblast' into Glow Squids")
+    }
+
+    init {
+        dualSeparator {
+            title = "Vanquishers"
+            description = "Settings for Vanquishers"
+        }
+    }
+
+    var vanquisherPartyMessages by reloadableBoolean(true) {
+        name = Literal("Vanquisher Party Message")
+        description = Literal("Sends a party message when a Vanquisher spawns.")
+    }
+
+    var vanquisherPartyMessageNoFishing by string("A Vanquisher has spawned! {coords}") {
+        name = Literal("Vanquisher Message (Not Fishing)")
+        description = Literal("The message sent when a Vanquisher spawns and you are not fishing. Params: {count}, {coords}")
+        condition = { vanquisherPartyMessages }
+    }
+
+    var vanquisherPartyMessageFishing by string("A Vanquisher has spawned! #{count} SCs") {
+        name = Literal("Vanquisher Message (Fishing)")
+        description = Literal("The message sent when a Vanquisher spawns and you are fishing. Params: {count}, {coords}")
+        condition = { vanquisherPartyMessages }
+    }
+
+    init {
+        previewButton(
+            VanquisherPartyMessage::previewNoFishing,
+            "Preview Message (Not Fishing)",
+            "Shows a preview of the vanquisher party message when not fishing in chat."
+        ) { vanquisherPartyMessages }
+
+        previewButton(
+            VanquisherPartyMessage::previewFishing,
+            "Preview Message (Fishing)",
+            "Shows a preview of the vanquisher party message when fishing in chat."
+        ) { vanquisherPartyMessages }
     }
 }
