0081b4590e49b97a74b554a47ef4905443a4a9e6
Riccio <riccio.fernando@gmail.com>
2026-09-20 14:49:50 -0300

    feat: add /kaboom command

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@
 - Added TPS Hud display (disabled by default)
 - Added Shard Hotspots support
 - Added :aquamarine: :carmine: :midnight: and :treasure: emojis
+- Added /kaboom command to send :kaboom: in party chat and leave the party
 
 ### Changes
 - Revoked previous backend acceptance to display privacy policy
---
+++ src/main/kotlin/cloud/glitchdev/rfu/feature/other/KaboomCommand.kt
@@ -0,0 +1,26 @@
+﻿package cloud.glitchdev.rfu.feature.other
+
+import cloud.glitchdev.rfu.constants.text.TextColor
+import cloud.glitchdev.rfu.utils.Chat
+import cloud.glitchdev.rfu.utils.Party
+import cloud.glitchdev.rfu.utils.TextUtils
+import cloud.glitchdev.rfu.utils.command.Command
+import cloud.glitchdev.rfu.utils.command.SimpleCommand
+import com.mojang.brigadier.context.CommandContext
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
+
+@Command
+object KaboomCommand : SimpleCommand("kaboom") {
+    override val description: String = "Sends :kaboom: in party chat and leaves the party."
+
+    override fun execute(context: CommandContext<FabricClientCommandSource>): Int {
+        if (!Party.inParty) {
+            context.source.sendFeedback(TextUtils.rfuLiteral("You are not in a party!", TextColor.LIGHT_RED))
+            return 1
+        }
+
+        Chat.sendPartyMessage(":kaboom:")
+        Chat.sendCommand("p leave")
+        return 1
+    }
+}
